91e35acba4251348aa498e6f1bc5325ee30277ec
Pochyxi <adienerlopez@gmail.com>
2022-11-20 01:05:27 +0100

    update

--- src/main/java/com/example/lastbuildweek/controllers/AuthController.java
+++ src/main/java/com/example/lastbuildweek/controllers/AuthController.java
@@ -13,10 +13,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,6 +21,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
 	
 	@Autowired
